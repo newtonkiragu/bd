491f7b5e8c9599933712dc5bb00ad5293e735e2c
newtonkiragu <karanunewton4@gmail.com>
2018-12-18 05:59:03 +0300

    added comment for debug

--- js/script.js
+++ js/script.js
@@ -1,20 +1,24 @@
-$(function() {
+$(function () {
 
     var flame = $('#flame');
     var txt = $('h1');
-  
+
     flame.on({
-      click: function() {
-        flame.removeClass('burn').addClass('puff');
-        $('.smoke').each(function() {
-          $(this).addClass('puff-bubble');
-        });
-        $('#glow').remove();
-        txt.hide().html("It <b>will</b> come true..").delay(750).fadeOut(300);
-        $('#candle').animate({
-          'opacity': '.5'
-        }, 100);
-      }
+        click: function () {
+            flame.removeClass('burn').addClass('puff');
+            console.log("off")
+            $('.smoke').each(function () {
+                $(this).addClass('puff-bubble');
+            });
+            console.log("smoking cold")
+            $('#glow').remove();
+            console.log("and after light, came darkness")
+            txt.show().html("It <b>will</b> come true..").delay(750).fadeOut(500);
+            console.log("have faith")
+            $('#candle').animate({
+                'opacity': '.5'
+            }, 100);
+        }
     })
-        
-  });
+
+});
