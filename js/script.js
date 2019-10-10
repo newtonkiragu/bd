748c3d57043a7497cc289d0981e360eba8365c9a
Newton Karanu <karanunewton4@gmail.com>
2019-10-10 20:35:28 +0300

    displayed canvas on candle off

--- js/script.js
+++ js/script.js
@@ -2,22 +2,28 @@ $(function () {
 
     var flame = $('#flame');
     var txt = $('#wish');
+    // var username = $('#username')
 
     flame.on({
         click: function () {
             flame.removeClass('burn').addClass('puff');
-            console.log("off")
+            console.log("off");
             $('.smoke').each(function () {
                 $(this).addClass('puff-bubble');
             });
-            console.log("smoking cold")
+            console.log("smoking cold");
             $('#glow').remove();
             console.log("and after light, came darkness")
-            txt.show().html("It <b>will</b> come true..").delay(750).fadeOut(500);
-            console.log("have faith")
+            txt.show().html("It <b>will</b> come true...").delay(750).fadeOut(750);
+            console.log(txt);
+            console.log("have faith");
             $('#candle').animate({
                 'opacity': '.5'
             }, 100);
+            // username.show().html("Happy Birthday " + name);
+            // console.log(username);
+            document.getElementById("myCanvas").style.display = "block";
+            document.getElementById('candle').style.display = "none";
         }
     })
 
