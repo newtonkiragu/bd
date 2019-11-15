c1f54200e82db958d3072803c749493a73d897e5
Newton Karanu <karanunewton4@gmail.com>
2019-11-15 16:01:06 +0300

    changed timelines for animations

--- js/script.js
+++ js/script.js
@@ -2,28 +2,26 @@ $(function () {
 
     var flame = $('#flame');
     var txt = $('#wish');
+    var txt2 = $('#wish1');
+    var myCanvas = $('#myCanvas');
+    var txt1 = document.getElementById("wish1");
     // var username = $('#username')
 
     flame.on({
         click: function () {
             flame.removeClass('burn').addClass('puff');
-            console.log("off");
             $('.smoke').each(function () {
                 $(this).addClass('puff-bubble');
             });
-            console.log("smoking cold");
             $('#glow').remove();
-            console.log("and after light, came darkness")
-            txt.show().html("It <b>will</b> come true...").delay(750).fadeOut(750);
-            console.log(txt);
-            console.log("have faith");
+            txt.show().html("It <b>will</b> come true...").fadeOut(2000);
             $('#candle').animate({
                 'opacity': '.5'
             }, 100);
-            // username.show().html("Happy Birthday " + name);
-            // console.log(username);
             document.getElementById("myCanvas").style.display = "block";
             document.getElementById('candle').style.display = "none";
+            txt2.fadeIn(3500);
+            // myCanvas.fadeIn(3500);
         }
     })
 
